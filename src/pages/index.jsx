8d30e89fa02df6cdafae29a62e8a37eb9404c2d0
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2023-03-05 00:58:44 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,8 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
+          Pratham Prasoon - Software developer, tweleb, and a computer scientist
+          in the making.
         </title>
         <meta
           name="description"
@@ -261,13 +262,10 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software developer, tweleb, and a computer scientist in the making.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            Yo, I'm Pratham!
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
